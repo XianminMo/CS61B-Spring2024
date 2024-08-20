396d62f1d24d352bf01e688a62e512f6fb55e577
Xianmin Mo <xianminmo@stu.xjtu.edu.cn>
2024-08-20 10:20:00 +0800

    WordNet done

--- proj2b/src/WordNet/WordNet.java
+++ proj2b/src/WordNet/WordNet.java
@@ -2,18 +2,34 @@ package WordNet;
 
 import edu.princeton.cs.algs4.In;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WordNet {
     private Graph graph;
-    private Map<Integer, List<String>> labelIndexMap;
+    private myMap labelIndexMap;
+
+    private class myMap extends HashMap<Integer, List<String>> {
+        public myMap() {
+            super();
+        }
+
+        public List<Integer> findIndexForWord(String word) {
+            List<Integer> indexes = new ArrayList<>();
+            for (Entry<Integer, List<String>> entry : this.entrySet()) {
+                int index = entry.getKey();
+                List<String> words = entry.getValue();
+                if (words.contains(word)) {
+                    indexes.add(index);
+                }
+            }
+            return indexes;
+        }
+
+    }
 
     public WordNet(String synsetsFilename, String hyponymsFilename) {
         graph = new Graph();
-        labelIndexMap = new HashMap<>();
+        labelIndexMap = new myMap();
 
         loadSynsetsFile(synsetsFilename, labelIndexMap);
         loadHyponymsFile(hyponymsFilename, graph);
@@ -45,8 +61,19 @@ public class WordNet {
         }
     }
 
-    public List<String> hyponyms(String word) {
-        List<Integer> vertexs = new ArrayList<>();
+    public Set<String> hyponyms(String word) {
+        List<Integer> indexes = labelIndexMap.findIndexForWord(word);
+        Set<String> hyponymsSet = new HashSet<>();
+        for (Integer index : indexes) {
+            Iterable<Integer> neighbors = graph.neighbors(index);
+            for (Integer neighbor : neighbors) {
+                List<String> neighborWords = labelIndexMap.get(neighbor);
+                if (neighborWords != null) {
+                    hyponymsSet.addAll(neighborWords);
+                }
+            }
+        }
+        return hyponymsSet;
     }
 
 
