98af14867c32d6430ac12b4af5b506229fda481b
xianminmo <1710519126@qq.com>
2024-08-23 17:01:16 +0800

    Added some tests (noWord, toBig)

--- proj2c/tests/TestOneWordKNot0Hyponyms.java
+++ proj2c/tests/TestOneWordKNot0Hyponyms.java
@@ -9,22 +9,45 @@ import java.util.List;
 import static com.google.common.truth.Truth.assertThat;
 
 public class TestOneWordKNot0Hyponyms {
-    public static final String WORDS_FILE = "data/ngrams/frequency-EECS.csv";
+    public static final String WORDS_FILE = "data/ngrams/top_14377_words.csv";
     public static final String TOTAL_COUNTS_FILE = "data/ngrams/total_counts.csv";
-    public static final String SMALL_SYNSET_FILE = "data/wordnet/synsets-EECS.txt";
-    public static final String SMALL_HYPONYM_FILE = "data/wordnet/hyponyms-EECS.txt";
+    public static final String SMALL_SYNSET_FILE = "data/wordnet/synsets.txt";
+    public static final String SMALL_HYPONYM_FILE = "data/wordnet/hyponyms.txt";
 
     @Test
     public void testActKNot0() {
         NgordnetQueryHandler studentHandler = AutograderBuddy.getHyponymsHandler(
                 WORDS_FILE, TOTAL_COUNTS_FILE, SMALL_SYNSET_FILE, SMALL_HYPONYM_FILE);
-        List<String> words = List.of("CS61A");
+        List<String> words = List.of("food", "cake");
 
-        NgordnetQuery nq = new NgordnetQuery(words, 2010, 2020, 4, NgordnetQueryType.HYPONYMS);
+        NgordnetQuery nq = new NgordnetQuery(words, 1950, 1990, 5, NgordnetQueryType.HYPONYMS);
         String actual = studentHandler.handle(nq);
-        String expected = "[CS170, CS61A, CS61B, CS61C]";
+        String expected = "[cake, cookie, kiss, snap, wafer]";
         assertThat(actual).isEqualTo(expected);
     }
 
-    // TODO: Add more unit tests (including edge case tests) here.
+    @Test
+    public void testNoWord() {
+        String wordsFile = "data/ngrams/very_short.csv";
+        NgordnetQueryHandler studentHandler = AutograderBuddy.getHyponymsHandler(
+                wordsFile, TOTAL_COUNTS_FILE, SMALL_SYNSET_FILE, SMALL_HYPONYM_FILE);
+        List<String> words = List.of("food");
+
+        NgordnetQuery nq = new NgordnetQuery(words, 1950, 1990, 5, NgordnetQueryType.HYPONYMS);
+        String actual = studentHandler.handle(nq);
+        String expected = "[]";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testKToBig() {
+        NgordnetQueryHandler studentHandler = AutograderBuddy.getHyponymsHandler(
+                WORDS_FILE, TOTAL_COUNTS_FILE, SMALL_SYNSET_FILE, SMALL_HYPONYM_FILE);
+        List<String> words = List.of("potato");
+
+        NgordnetQuery nq = new NgordnetQuery(words, 1950, 1990, 15, NgordnetQueryType.HYPONYMS);
+        String actual = studentHandler.handle(nq);
+        String expected = "[chips, potato]";
+        assertThat(actual).isEqualTo(expected);
+    }
 }
